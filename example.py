f34b666c56067193c6960c88a52fc2156c2bf70f
Beldon <beldonlin@gmail.com>
2021-02-05 13:51:34 -0800

    Added demo python scripts
    example.py - Fibonnaci
    test.py - hello world

---
+++ example.py
@@ -0,0 +1,17 @@
+# Demo fibonnaci sequence
+
+num_seq = 10
+
+F1 = 1
+F0 = 0
+
+for i in range(num_seq):
+    F2 = F1 + F0
+
+    # shift numbers
+    io
+    F0 = F1
+    F1 = F2
+    print(F2)
+
+    
